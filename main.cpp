9db3238752386c6376d8b7911d512ec18bec5a60
khaledmraad <khaled.mrad@ensi-uma.tn>
2023-12-18 11:37:18 +0100

    Singleton design pattern

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     EBank* e=EBank::getInstance("this is our EBank");
-    EBank* b=EBank::getInstance("this is our second EBank");
+    EBank* b=EBank::getInstance("this is our second EBank"); //won't work
 
 
     return 0;
